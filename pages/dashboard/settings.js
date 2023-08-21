9ea968a446825556218c617038c7f171fbaddf4d
kent <nwekep14@gmail.com>
2023-08-21 14:56:39 +0100

    re add settings page

---
+++ pages/dashboard/settings.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Settings } from "../../app/modules/dashboard/settings/Settings";
+
+export default function SettingsPage() {
+	return (
+		<React.Fragment>
+			<Settings />
+		</React.Fragment>
+	);
+}
